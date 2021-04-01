8745880a92bf76c7add3a2c0259372cfe4269bd8
abirhasanmubin <abirhasanmubin@gmail.com>
2021-04-01 22:17:01 +0600

    added navigation and router

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 import { Hero } from '../hero';
-
+import { HeroService } from '../hero.service';
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero?: Hero;
+    hero: Hero;
 
-  constructor() { }
+    constructor(
+        private route: ActivatedRoute,
+        private heroService: HeroService,
+        private location: Location
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getHero();
+    }
+
+    getHero(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.heroService.getHero(id)
+            .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,16 +8,23 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HeroService {
 
-  // constructor() { }
-  constructor(private messageService: MessagesService) { }
+    // constructor() { }
+    constructor(private messageService: MessagesService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-  }
+    getHeroes(): Observable<Hero[]> {
+        const heroes = of(HEROES);
+        this.messageService.add('HeroService: fetched heroes');
+        return heroes;
+    }
+
+    getHero(id: number): Observable<Hero> {
+        const hero = HEROES.find(h => h.id === id) as Hero;
+        this.messageService.add(`HeroService: fetched hero id=${id}`);
+        return of(hero);
+    }
+
 }
